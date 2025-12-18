fcc02f3909f2751c8b646653e8d788c4413eb0a4
Alone <hi@anlo.ng>
2025-12-18 15:40:14 +0800

    Fix scrcpy adbkey

--- ws-scrcpy/rootfs/run.sh
+++ ws-scrcpy/rootfs/run.sh
@@ -7,6 +7,10 @@ set -ex
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/http.d/*.conf
 nginx -g "error_log /dev/stdout info;"
 
+if [ ! -d "$ANDROID_HOME" ]; then
+    mkdir -p "$ANDROID_HOME"
+fi
+
 CONFIG_PATH=/data/options.json
 jq -r '.hosts[]' $CONFIG_PATH | while IFS=':' read -r addr port; do
     adb connect $addr:$port
